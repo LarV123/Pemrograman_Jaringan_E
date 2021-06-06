82fa2a29d1d42194a10bc0b20c4971b8992d6b5f
LarV123 <geizka.fahriza@gmail.com>
2021-06-06 20:45:13 +0700

    tugas 4 complete

---
+++ progjar4a/tugas_client.py
@@ -0,0 +1,30 @@
+from file_client_cli import send_command
+import datetime
+import threading
+
+def execute_get(id):
+    send_command("GET pokijan.jpg")
+    print(f"request {id}")
+
+def request_file_pokijan(count=100):
+    texec = dict()
+
+    catat_awal = datetime.datetime.now()
+    for i in range(0,count):
+        print(f"request number {i} of pokijan.jpg")
+        texec[i] = threading.Thread(target=execute_get, args=(i))
+        texec[i].start()
+
+    #setelah menyelesaikan tugasnya, dikembalikan ke main thread dengan join
+    for i in range(0, count):
+        texec[i].join()
+
+    catat_akhir = datetime.datetime.now()
+    selesai = catat_akhir - catat_awal
+    print(f"Waktu TOTAL yang dibutuhkan {selesai} detik {catat_awal} s/d {catat_akhir}")
+
+
+#fungsi download_gambar akan dijalankan secara multithreading
+
+if __name__=='__main__':
+    request_file_pokijan()
