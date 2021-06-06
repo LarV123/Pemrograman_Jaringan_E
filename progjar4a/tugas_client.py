c4a1348ec4f6c4c5d98e688103aaa069147e9083
LarV123 <geizka.fahriza@gmail.com>
2021-06-06 14:07:28 +0000

    fix error

--- progjar4a/tugas_client.py
+++ progjar4a/tugas_client.py
@@ -4,7 +4,7 @@ import threading
 
 def execute_get(id):
     send_command("GET pokijan.jpg")
-    print(f"request {id}")
+    print(f"request {id} complete")
 
 def request_file_pokijan(count=100):
     texec = dict()
@@ -12,12 +12,12 @@ def request_file_pokijan(count=100):
     catat_awal = datetime.datetime.now()
     for i in range(0,count):
         print(f"request number {i} of pokijan.jpg")
-        texec[i] = threading.Thread(target=execute_get, args=(i))
-        texec[i].start()
+        texec[f"{i}"] = threading.Thread(target=execute_get, args=(i,))
+        texec[f"{i}"].start()
 
     #setelah menyelesaikan tugasnya, dikembalikan ke main thread dengan join
     for i in range(0, count):
-        texec[i].join()
+        texec[f"{i}"].join()
 
     catat_akhir = datetime.datetime.now()
     selesai = catat_akhir - catat_awal
